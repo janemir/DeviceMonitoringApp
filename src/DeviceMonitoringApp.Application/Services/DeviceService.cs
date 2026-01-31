f713aa7ac305a8cc2d308d96c2939371f13edd6d
janemir <janemiro2005@gmail.com>
2026-01-31 21:49:37 +0300

    clean up comments

--- src/DeviceMonitoringApp.Application/Services/DeviceService.cs
+++ src/DeviceMonitoringApp.Application/Services/DeviceService.cs
@@ -7,8 +7,6 @@ public class DeviceService(IDeviceRepository deviceRepository) : IDeviceService
 {
     public Task<Guid> AddAsync(Device device)
     {
-        // For now the application layer just delegates to the repository.
-        // Here we could later add validation, mapping, logging, etc.
         return deviceRepository.AddAsync(device);
     }
 
